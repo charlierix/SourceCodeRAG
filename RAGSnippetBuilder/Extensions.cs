cf5a729f1d18a9437164638534ded2420f130655
Charlie Rix <charlie.rix@gmail.com>
2024-09-22 20:20:30 -0500

    wired in the line parser

---
+++ RAGSnippetBuilder/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAGSnippetBuilder
+{
+    public static class Extensions
+    {
+        public static string ToJoin(this IEnumerable<string> values, string separator)
+        {
+            return string.Join(separator, values);
+        }
+    }
+}
