497bcfaec8a5d4f4d8fb8f7572aaa30cf245b3c3
charlierix <charlie.rix@gmail.com>
2024-10-06 14:13:28 -0500

    got it working with multiple simultaneous instances

--- ChromaTest2/basic_flask2.py
+++ ChromaTest2/basic_flask2.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify, url_for
-from werkzeug.serving import make_server
+import socket
 import threading
 import time
+from flask import Flask, request, jsonify, url_for
+from werkzeug.serving import make_server
 
 # Call this from command line to send messages to this script
 # curl -X POST http://localhost:5000/stop
@@ -29,17 +30,20 @@ if __name__ == '__main__':
     ip = '127.0.0.1'
     port = 5000
 
-    for _ in range(4000):
+    while True:
         try:
-            server = make_server(ip, port, app)
-            print('* Running on http://' + ip + ':' + str(port))
+            # Create a socket and bind it to the desired IP address and port
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.bind((ip, port))
+            sock.close()        # need to close, or the call to make_server will fail
+            print(f'found free port: {port}')
             break
-        except OSError as e:
-            if e.errno == 98: # Address already in use
-                print(f"Port {port} is already in use. Trying next one.")
-                port += 1
-            else:
-                raise
+        except socket.error as e:
+            print(f"Port {port} is already in use, trying next one...")
+            port += 1
+
+    server = make_server(ip, port, app)
+    print(f'* Running on http://{ip}:{port}')
 
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.start()
